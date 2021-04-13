e0ef6cc9fb1aca608693bf6fbf9fa8e236825c34
vnescape <vnescape@protonmail.com>
2021-04-13 13:37:25 +0200

    Playing with GlBuffer It is not working due to linking errors.

--- Space-Invaders/Space-Invaders/src/main.cpp
+++ Space-Invaders/Space-Invaders/src/main.cpp
@@ -25,17 +25,24 @@ int main(void)
     // Print current OpenGL  version
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f,
+    };
+
+    unsigned int BufferID;
+    glGenBuffers(1, &BufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, BufferID);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
